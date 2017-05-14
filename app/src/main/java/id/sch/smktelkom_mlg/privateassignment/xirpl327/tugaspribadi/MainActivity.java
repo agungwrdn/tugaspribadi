a4db021140926e7f794b63e2a0139853a21cb772
bang rawon <agungwar00@gmail.com>
2017-05-14 11:38:18 +0700

    navigation

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/MainActivity.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +14,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.fragment.FavoriteFragment;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.fragment.HomeFragment;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.fragment.PopularFragment;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.fragment.UpcomingFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -22,15 +28,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -79,21 +76,18 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment sekolah = null;
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            sekolah = new HomeFragment();
         } else if (id == R.id.nav_gallery) {
-
+            sekolah = new PopularFragment();
         } else if (id == R.id.nav_slideshow) {
-
+            sekolah = new UpcomingFragment();
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            sekolah = new FavoriteFragment();
         }
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, sekolah).commitNow();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/adapter/upcoming.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.adapter;
+
+/**
+ * Created by root on 14/05/17.
+ */
+
+public class upcoming {
+}
