847d56b7f499d3fb9eb5537372f3ee1c40b769a9
bang rawon <agungwar00@gmail.com>
2017-05-14 13:38:35 +0700

    Volley dilapangan liat film

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/MainActivity.java
@@ -1,10 +1,8 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -37,6 +35,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changePage(R.id.nav_home);
+        navigationView.setCheckedItem(R.id.nav_home);
     }
 
     @Override
@@ -76,20 +77,27 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        changePage(id);
+
+
+        return true;
+    }
+
+    private void changePage(int id) {
+
         Fragment sekolah = null;
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             sekolah = new HomeFragment();
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_popular) {
             sekolah = new PopularFragment();
-        } else if (id == R.id.nav_slideshow) {
-            sekolah = new UpcomingFragment();
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_upcoming) {
             sekolah = new FavoriteFragment();
+        } else if (id == R.id.nav_favorite) {
+            sekolah = new UpcomingFragment();
         }
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, sekolah).commitNow();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/fragment/PopularFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/fragment/PopularFragment.java
@@ -3,11 +3,25 @@ package id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.R;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.adapter.popular;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.model.Results;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.model.ResultsRespons;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.service.GsonGetRequest;
+import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.service.VolleySingleton;
 
 
 /**
@@ -16,16 +30,49 @@ import id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.R;
 public class PopularFragment extends Fragment {
 
 
-    public PopularFragment() {
-        // Required empty public constructor
-    }
-
+    ArrayList<Results> mlist = new ArrayList<>();
+    popular myPopular;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_popular, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_popular, container, false);
+
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view_popular);
+        rv.setHasFixedSize(true);
+        myPopular = new popular(this, mlist, getContext());
+        rv.setAdapter(myPopular);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        downloadDataResource();
+
+        return rootView;
     }
 
+    private void downloadDataResource() {
+        String url = "https://api.themoviedb.org/3/movie/popular?api_key=f5203c197b2af435b78315acb9ae10b6&language=en-US&page=1";
+
+        GsonGetRequest<ResultsRespons> myRequest = new GsonGetRequest<ResultsRespons>
+                (url, ResultsRespons.class, null, new Response.Listener<ResultsRespons>() {
+
+                    @Override
+                    public void onResponse(ResultsRespons response) {
+                        Log.d("FLOW", "onResponse: " + (new Gson().toJson(response)));
+                        mlist.addAll(response.results);
+                        myPopular.notifyDataSetChanged();
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("FLOW", "onErrorResponse: ", error);
+                    }
+                });
+        VolleySingleton.getInstance(this).addToRequestQueue(myRequest);
+    }
 }
